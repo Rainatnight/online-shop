10c589cac6ad0cca938446c9ed0d3d17cb3c8965
Rainatnight <86172403+Rainatnight@users.noreply.github.com>
2021-07-14 14:17:03 +0300

    Update cars_catalog.php

--- online-shop/templates/cars_catalog.php
+++ online-shop/templates/cars_catalog.php
@@ -2,7 +2,7 @@
   <div class="py-4 pb-8">
     <h1 class="text-black text-3xl font-bold mb-4">Каталог</h1>
     <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6">
-      <?php include ("/home/CREAT0RS/nagornyi.a_qschool/p3/htdocs/templates/car_item.php") ?>
+      <?php include ("/templates/car_item.php") ?>
     </div>
   </div>
 </main>
